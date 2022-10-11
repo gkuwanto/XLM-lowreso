77fbce6c42fd828c1a45de03beba5e3df3ca5215
Garry Kuwanto <gkuwanto@gmail.com>
2022-10-11 18:04:30 -0400

    debug

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -504,7 +504,7 @@ class TransformerModel(nn.Module):
 
         # Calculate Loss
         loss_orig = (-torch.log(sim_pair_orig.diag() /
-                                (sim_pair_orig-sim_pair_orig.diag().diag()))).sum()
+                                (sim_pair_orig))).sum(dim=1)
         if torch.any(torch.isnan(loss_orig)):
             print("loss_orig")
         if has_cs:
